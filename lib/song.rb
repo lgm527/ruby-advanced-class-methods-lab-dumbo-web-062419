0e02a4068b922e8dc14941208544cbe9dbff8d3c
lgm527 <laurellmccaffrey@gmail.com>
2019-06-27 19:04:49 -0400

    song class developed

--- lib/song.rb
+++ lib/song.rb
@@ -2,12 +2,71 @@ class Song
   attr_accessor :name, :artist_name
   @@all = []
 
+  def initialize(name=nil)
+      @name = name
+      @artist_name = artist_name
+  end
+
   def self.all
     @@all
   end
 
   def save
     self.class.all << self
+  end
+
+  def self.create(name=nil)
+    new_song = Song.new(name)
+    new_song.save
+    new_song
+  end
+
+  def self.new_by_name(name)
+    new_song = Song.new(name)
+  end
+
+  def self.create_by_name(name)
+    new_song = Song.create(name)
+  end
+
+  def self.find_by_name(title)
+    Song.all.find do |song|
+      song.name == title
+    end
+  end
+
+  def self.find_or_create_by_name(name)
+    if self.find_by_name(name) != nil
+      self.find_by_name(name)
+    else
+      self.create_by_name(name)
+    end
+  end
+
+  def self.alphabetical
+    self.all.sort_by! { |song| song.name }
+  end
+
+  def self.new_from_filename(file)
+    split_file = file.split(" - ")
+    a_n = split_file[0]
+    n = split_file[1].sub('.mp3','')
+    new_song = Song.new(n)
+    new_song.artist_name = a_n
+    new_song
+  end
+
+  def self.create_from_filename(file)
+    split_file = file.split(" - ")
+    a_n = split_file[0]
+    n = split_file[1].sub('.mp3','')
+    new_song = Song.create(n)
+    new_song.artist_name = a_n
+    new_song
+  end
+
+  def self.destroy_all
+    self.all.clear
   end
 
 end
